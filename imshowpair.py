1078dbacfd16a74a1bc79a135d965b0952046ce2
Lev E. Givon <lev@columbia.edu>
2018-10-26 15:34:05 -0400

    Add cmap option, make plots share axes.

--- imshowpair.py
+++ imshowpair.py
@@ -11,7 +11,8 @@ import numpy as np
 def blend(a, b, cmap=plt.cm.gray, alpha=0.5, interpolation='nearest'):
     raise NotImplementedError
 
-def imshowpair(a, b, method=None, show_all=True, *args, **kwargs):
+def imshowpair(a, b, method=None, show_all=True, axes_visible=False, cmap=None,
+            *args, **kwargs):
     """
     Compare images.
 
@@ -26,6 +27,10 @@ def imshowpair(a, b, method=None, show_all=True, *args, **kwargs):
     show_all : bool
         If True and `method` is defined, display original images
         alongside `method(a, b)`.
+    axes_visible : bool
+        Show axes if True.
+    cmap : matplotlib.colors.Colormap
+        Color map to use when displaying images.
     """
 
     def int_axes(ax, shape):
@@ -37,38 +42,50 @@ def imshowpair(a, b, method=None, show_all=True, *args, **kwargs):
     a_shape = a.shape
     b_shape = b.shape
 
+    ax_list = []
     if method == None:
-        ax = plt.subplot(121)
-        plt.imshow(a, origin='upper')
+        ax0 = ax = plt.subplot(121)
+        plt.imshow(a, origin='upper', cmap=cmap)
         int_axes(ax, a_shape)
-        ax = plt.subplot(122)
-        plt.imshow(b, origin='upper')
+        ax_list.append(ax)
+        ax = plt.subplot(122, sharex=ax0, sharey=ax0)
+        plt.imshow(b, origin='upper', cmap=cmap)
         int_axes(ax, b_shape)
         if a_shape == b_shape:
             ax.axes.yaxis.set_visible(False)
+        ax_list.append(ax)
         plt.subplots_adjust()
     elif callable(method):
         c = method(a, b, *args, **kwargs)
         c_shape = c.shape
         if show_all:
-            ax = plt.subplot(131)
-            plt.imshow(a, origin='upper')
+            ax0 = ax = plt.subplot(131)
+            plt.imshow(a, origin='upper', cmap=cmap)
             int_axes(ax, a_shape)
-            ax = plt.subplot(132)
-            plt.imshow(b, origin='upper')
+            ax_list.append(ax)
+            ax = plt.subplot(132, sharex=ax0, sharey=ax0)
+            plt.imshow(b, origin='upper', cmap=cmap)
             int_axes(ax, b_shape)
             if a_shape == b_shape:
                 ax.axes.yaxis.set_visible(False)
-            ax = plt.subplot(133)
-            plt.imshow(c, origin='upper')
+            ax_list.append(ax)
+            ax = plt.subplot(133, sharex=ax0, sharey=ax0)
+            plt.imshow(c, origin='upper', cmap=cmap)
             int_axes(ax, c_shape)
             plt.subplots_adjust()
             if c_shape == b_shape:
                 ax.axes.yaxis.set_visible(False)
+            ax_list.append(ax)
         else:
-            plt.imshow(c, origin='upper')
+            plt.imshow(c, origin='upper', cmap=cmap)
             int_axes(ax, c_shape)
             if c_shape == b_shape:
                 ax.axes.yaxis.set_visible(False)
+            ax_list.append(ax)
     else:
         NotImplementedError
+    if not axes_visible:
+        for ax in ax_list:
+            ax.axes.xaxis.set_visible(False)
+            ax.axes.yaxis.set_visible(False)
+
